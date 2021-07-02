91748e8135007bff4179b2ad651fd2bd8e34c427
13-012020 <86288845+13-012020@users.noreply.github.com>
2021-07-02 14:18:37 +0530

    Add files via upload

---
+++ csquiz.py
@@ -0,0 +1,292 @@
+print("____________________________________________________________________")
+print("********************************************************************")
+print("************** sustainable development goals quiz ******************")
+print("********************************************************************")
+print("_____________________________________________________________________")
+print ("lets start the quiz!!!!")
+count=0
+print(" Q1 Which of the following is not true about the SDGs?")#1
+print("(A)They encourage the promotion of health, well-being, and education for all, at all ages\n(B)They include the development of sustainable cities, infrastructure, and industry")
+print("(C)They are a legally binding international treaty that all nations are required to follow\n(D)They explicitly promote innovation")
+n1=input("enter you answer(in caps)-")
+if n1=="C":
+    print("your answer is correct!!!")
+    count+=1
+else:
+    print("your answer is wrong :(")
+    print("the correct answer is (C)They are a legally binding international treaty that all nations are required to follow ")
+print("_____________________________________________________________________")
+print("Q2 To which countries are the sustainable development goals designed to apply?")#2
+print("(A)Low- and middle income countries, not high income countries\n(B)All countries")
+print("(C)Only countries where more than 50% of the population live below the poverty line\n(D)Only countries in Sub-Saharan Africa")
+n2=input("enter your answer(in caps)-")
+if n2=="B":
+    print("your answer is correct!!!")
+    count+=1
+else:
+    print("your answer is wrong :(")
+    print("correct answer is (B)All countries")
+print("_____________________________________________________________________")
+print("Q3 Which of the following is not a sustainable development goal?")#3
+print("(A)Eliminate racism in all its forms\n(B)Climate action")
+print("(C)Clean water and sanitation\n(D)Good health and well-being")
+n3=input("enter your answer(in caps)-")
+if n3=="A":
+    print("your answer is correct!!!")
+    count+=1
+else:
+    print("your answer is wrong :(")
+    print("correct answer is (A)Eliminate racism in all its forms")
+print("_____________________________________________________________________")
+print("Q4 Each goal is supported by a set of “Targets” — specific objectives that are associated with that Goal. How many targets are there in total?")#4
+print("(A)150\n(B)99\n(C)169\n(D)50")
+n4=input("enter your answer(in caps)-")
+if n4=="C":
+    print("your answer is correct!!!")
+    count+=1
+else:
+    print("your answer is wrong :(")
+    print("the corect answer is (C)169")
+print("_____________________________________________________________________")
+print("Q5 How many sustainable development goals are there?")#5
+print("(A)15\n(B)20\n(C)17\n(D)8")
+n5=input("enter your answer(in caps)-")
+if n5=="C":
+    print("your answer is correct!!!")
+    count+=1
+else:
+    print("your answer is wrong:(")
+    print("correct answer is (C)17")
+print("_____________________________________________________________________")
+print("Q6 The 2030 Agenda and its sustainable development goals and targets came into effect on 1 January 2016. When are they due to expire?")#6
+print("(A)2020\n(B)2030\n(C)2026\n(D)2100")
+n6=input("enter your answer(in caps)-")
+if n6=="B":
+    print("your answer is correct!!!")
+    count+=1
+else:
+    print("your answer is wrong:(")
+    print("the correct answer is (B)2030")
+print("_____________________________________________________________________")
+print("Q7 What does the term 'sustainable' mean?")#7
+print("(A)The implementation of eco-friendly technologies.\n(B) A balance between meeting today's needs with those of the future.\n(C) The use of alternative energies.\n(D)all of the above")
+n7=input("enter your answer(in caps)-")
+if n7=="B":
+    print("your answer is correct!!!")
+    count+=1
+else:
+    print("your answer is wrong:(")
+    print("correct answer is (B) A balance between meeting today's needs with those of the future.")
+print("_____________________________________________________________________")
+print("Q8 Who are those 'left behind' referred to in the 2030 Agenda for Sustainable Development?")#8
+print("(A)The vulnerable groups.\n(B) The minorities.\n(C) Those without access to certain rights, goods and services.\n(D)all of the above")
+n8=input("enter your answer(in caps)-")
+if n8=="C":
+    print("your answer is correct!!!")
+    count+=1
+else:
+    print("your answer is wrong:(")
+    print("correct answer is (C) Those without access to certain rights, goods and services.")
+print("_____________________________________________________________________")
+print("Q9 The Voluntary National Reviews of the SDGs are a exclusive domain of governments?")#9
+print("(A)No, they must incorporate other stakeholders.\n(B) Yes, they are.\n(C) Yes, although consultations with other actors might take place.")
+n9=input("enter your answer(in caps)-")
+if n9=="C":
+    print("your answer is correct!!!")
+    count+=1
+else:
+    print("your answer is wrong:(")
+    print("correct answer is (C) Yes, although consultations with other actors might take place")
+print("_____________________________________________________________________")
+print("Q10 Why is the education for sustainable development relevant?")#10
+print(" (A)It enhances careers prospects\n(B) It promotes better understanding about the environment.\n(C) It empowers individuals to reflect on their own actions.\n(D)all of the above")
+n10=input("enter your answer(in caps)-")
+if n10=="C":
+    print("your answer is correct!!!")
+    count+=1
+else:
+    print("your answer is wrong:(")
+    print("correct answer is (C) It empowers individuals to reflect on their own actions")
+print("_____________________________________________________________________")
+print("Q11 How many people in the WHO European Region lack access to a basic drinking-water source?")#11
+print("(A)14 million\n(B)22 million\n(C)6 million\n(D)2 million")
+n11=input("enter your answer(in caps)-")
+if n11=="A":
+    print("your answer is correct!!!")
+    count+=1
+else:
+    print("your answer is wrong:(")
+    print("correct answer is (A)14 million")
+print("_____________________________________________________________________")
+print("Q12 Which energy-efficient forms of active transport reduce rates of obesity, diabetes, coronary heart disease, stroke and traffic injuries?")#12
+print("(A)Cycling\n(B)Walking\n(C)all of the above")
+n12=input("enter your answer(in caps)-")
+if n12=="C":
+    print("your answer is correct!!!")
+    count+=1
+else:
+    print("your answer is wrong:(")
+    print("correct answer is (C)all of the above")
+print("_____________________________________________________________________")
+print("Q13 How do the activities of health systems put pressure on the environment?")#13
+print("(A)By generating hazardous and conventional waste, as well as wastewater\n(B)By consuming resources, e.g., water and energy, and producing greenhouse gas emissions\n(C)all of the above")
+n13=input("enter your answer(in caps)-")
+if n13=="C":
+    print("your answer is correct!!!")
+    count+=1
+else:
+    print("your answer is wrong:(")
+    print("correct answer is (C)all of the above")
+print("_____________________________________________________________________")
+print("Q14 Which European diet, recommended as a healthy diet to follow, has lean and fatty fish at its centre?")#14
+print("(A)Nordic diet\n(B)Paleo diet\n(C)Gluten-free diet")
+n14=input("enter your answer(in caps)-")
+if n14=="A":
+    print("your answer is correct!!!")
+    count+=1
+else:
+    print("your answer is wrong:(")
+    print("correct answer is (A)Nordic diet")
+print("_____________________________________________________________________")
+print("Q15 What have studies shown to be the health benefits for people living in urban areas with more green space?")#15
+print("(A)Reduced level of stress\n(B)Lower likelihood of having type 2 diabetes\n(C)All of the above")
+n15=input("enter your answer(in caps)-")
+if n15=="C":
+    print("your answer is correct!!!")
+    count+=1
+else:
+    print("your answer is wrong:(")
+    print("correct answer is (C)All of the above")
+print("_____________________________________________________________________")
+print("Q16 What were the deadliest extreme weather events in 1991–2015 in Europe?")#16
+print("(A)Floods\n(B)Heat waves\n(C)Earthquakes")
+n16=input("enter your answer(in caps)-")
+if n16=="B":
+    print("your answer is correct!!!")
+    count+=1
+else:
+    print("your answer is wrong:(")
+    print("correct answer is (B)Heat waves")
+print("_____________________________________________________________________")
+print("Q17 By how many years has life expectancy in the WHO European Region risen in the past 20 years?")#17
+print("(A)2.2 years\n(B)3.7  years\n(C)4.6 years")
+n17=input("enter your answer(in caps)-")
+if n17=="C":
+    print("your answer is correct!!!")
+    count+=1
+else:
+    print("your answer is wrong:(")
+    print("correct answer is (C)4.6 years")
+print("_____________________________________________________________________")
+print("Q18 How long has WHO been working to achieve its objective of, as stated in the WHO Constitution, “the attainment by all peoples of the highest possible level of health”?")#18
+print("(A)50 years\n(B)60 years\n(C)70 years")
+n18=input("enter your answer(in caps)-")
+if n18=="C":
+    print("your answer is correct!!!")
+    count+=1
+else:
+    print("your answer is wrong:(")
+    print("correct answer is (C)70 years")
+print("_____________________________________________________________________")
+print("Q19 What percentage of European countries have a health promoting school policy included in the national education policy?")#19
+print("(A)30%\n(B)62% \n(C)78%")
+n19=input("enter your answer(in caps)-")
+if n19=="B":
+    print("your answer is correct!!!")
+    count+=1
+else:
+    print("your answer is wrong:(")
+    print("correct answer is (B)62%")
+print("_____________________________________________________________________")
+print("Q20 One extra year of life expectancy has been shown to raise gross domestic product (GDP) per capita by how much?")#20
+print("(A)1%\n(B)4%\n(C)7%")
+n20=input("enter your answer(in caps)-")
+if n20=="B":
+    print("your answer is correct!!!")
+    count+=1
+else:
+    print("your answer is wrong:(")
+    print("correct answer is (B)4%")
+print("_____________________________________________________________________")
+print("your scores are",count,"/20")
+if count>26:
+    print("outstanding")
+elif count>19:
+    print("good")
+elif count>9:
+    print("you need to learn more about SDG")
+else:
+    print("poor performance")
+print("_____________________________________________________________________")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+
+
+    
+    
+
+
+
+
+
+    
+    
+
+
+
+
+    
+
+
+
+    
+
+
+
+
+
+
+    
+    
+
+
+    
+
+
+
+    
+    
+
+
+
+
+
+
+
+
+
+    
+    
+
+    
+    
+    
